1904cc0d7152cd393d0229a89907fd2bb10ce7a5
zazdergod <buzyrevilya@mail.ru>
2020-09-13 16:23:31 +0300

    Second assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,32 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## function for make cache matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 	reverse <- NULL
-	set <- function(y) {
-		x <<- y 
-		reverse <<- NULL
+	set <- function(y) { ##set new values for matrix
+		x <<- y ##set new arguments for matrix
+		reverse <<- NULL ##set null to reverse matrix
 	}
-	get <- function() x
-	setReverse <- function(rev_mat) reverse <<- rev_mat
-	getReverse <- function() reverse
-	list(set = set, get = get, setReverse = setReverse, getReverse = getReverse)
+	get <- function() x ##get default matrix
+	setReverse <- function(rev_mat) reverse <<- rev_mat ##set new value for reverse matrix
+	getReverse <- function() reverse ##get reverse matrix value
+	list(set = set, get = get, setReverse = setReverse, getReverse = getReverse) ##return list of functions for updating values of cache
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        reverse <- x$getReverse()
-        if(!is.null(reverse)) {
+        reverse <- x$getReverse()##get value of reverse matrix
+        if(!is.null(reverse)) { ##check if something is stored in reverse matrix cache
         	message("getting cached data")
         	return(reverse)
         }
-        data <- x$get()
-        reverse <- solve(data)
-        x$setReverse(reverse)
-        reverse
+        data <- x$get() ##get value of default matrix
+        reverse <- solve(data)##making reverse matrix
+        x$setReverse(reverse)##set new value in reverse matrix cache
+        reverse ##return reverse matrix
 }
